e5c4ca9da45d5b0be5c34b66403e9b0240664466
Catalin <boja.catalin@hotmail.com>
2021-12-16 18:47:14 +0200

    Implementare bitonic sort

--- grupa 1/Laborator_12/Source.cpp
+++ grupa 1/Laborator_12/Source.cpp
@@ -104,8 +104,161 @@ void sortareOddEvenParalel(int* v, int N) {
 	}
 }
 
+
+//Merge sort
+//https://www.tutorialspoint.com/cplusplus-program-to-implement-merge-sort
+
+void swapping(int& a, int& b) {     //swap the content of a and b
+	int temp;
+	temp = a;
+	a = b;
+	b = temp;
+}
+void display(int* array, int size) {
+	for (int i = 0; i < size; i++)
+		cout << array[i] << " ";
+	cout << endl;
+}
+void merge(int* array, int l, int m, int r) {
+	int i, j, k;
+	//size of left and right sub-arrays
+	const int nl = m - l + 1; 
+	const int nr = r - m;
+	int* larr = new int[nl];
+	int* rarr = new int[nr];
+	//fill left and right sub-arrays
+	for (i = 0; i < nl; i++)
+		larr[i] = array[l + i];
+	for (j = 0; j < nr; j++)
+		rarr[j] = array[m + 1 + j];
+	i = 0; j = 0; k = l;
+	//marge temp arrays to real array
+	while (i < nl && j < nr) {
+		if (larr[i] <= rarr[j]) {
+			array[k] = larr[i];
+			i++;
+		}
+		else {
+			array[k] = rarr[j];
+			j++;
+		}
+		k++;
+	}
+	while (i < nl) {       //extra element in left array
+		array[k] = larr[i];
+		i++; k++;
+	}
+	while (j < nr) {     //extra element in right array
+		array[k] = rarr[j];
+		j++; k++;
+	}
+
+	delete[] larr;
+	delete[] rarr;
+}
+void mergeSort(int* array, int l, int r) {
+	int m;
+	if (l < r) {
+		int m = l + (r - l) / 2;
+		// Sort first and second arrays
+		mergeSort(array, l, m);
+		mergeSort(array, m + 1, r);
+		merge(array, l, m, r);
+	}
+}
+
+void sortareMerge(int* valori, int N) {
+	mergeSort(valori, 0, N - 1);
+}
+
+//bitonic sort - secvential
+//https://people.cs.rutgers.edu/~venugopa/parallel_summer2012/bitonic_openmp.html
+
+int m;
+#define MAX(A, B) (((A) > (B)) ? (A) : (B))
+#define MIN(A, B) (((A) > (B)) ? (B) : (A))
+#define UP 0
+#define DOWN 1
+
+void bitonic_sort_seq(int start, int length, int* seq, int flag)
+{
+	int i;
+	int split_length;
+
+	if (length == 1)
+		return;
+
+	if (length % 2 != 0)
+	{
+		printf("error\n");
+		exit(0);
+	}
+
+	split_length = length / 2;
+
+	// bitonic split
+	for (i = start; i < start + split_length; i++)
+	{
+		if (flag == UP)
+		{
+			if (seq[i] > seq[i + split_length])
+				swap(seq[i], seq[i + split_length]);
+		}
+		else
+		{
+			if (seq[i] < seq[i + split_length])
+				swap(seq[i], seq[i + split_length]);
+		}
+	}
+
+	bitonic_sort_seq(start, split_length, seq, flag);
+	bitonic_sort_seq(start + split_length, split_length, seq, flag);
+}
+
+void bitonic_sort_par(int start, int length, int* seq, int flag)
+{
+	int i;
+	int split_length;
+
+	if (length == 1)
+		return;
+
+	if (length % 2 != 0)
+	{
+		printf("The length of a (sub)sequence is not divided by 2.\n");
+		exit(0);
+	}
+
+	split_length = length / 2;
+
+	// bitonic split
+#pragma omp parallel for shared(seq, flag, start, split_length) private(i)
+	for (i = start; i < start + split_length; i++)
+	{
+		if (flag == UP)
+		{
+			if (seq[i] > seq[i + split_length])
+				swap(seq[i], seq[i + split_length]);
+		}
+		else
+		{
+			if (seq[i] < seq[i + split_length])
+				swap(seq[i], seq[i + split_length]);
+		}
+	}
+
+	if (split_length > m)
+	{
+		// m is the size of sub part-> n/numThreads
+		bitonic_sort_par(start, split_length, seq, flag);
+		bitonic_sort_par(start + split_length, split_length, seq, flag);
+	}
+
+	return;
+}
+
 int main() {
-	const int N = 1e5;
+	const int N = 2^17;		//avem nevoie pentru bitonic sort
 	int* valori = generare(N);
 	int* copie = copiere(valori, N);
 
@@ -117,5 +270,14 @@ int main() {
 	//Durata sortare = 53.0702
 	//benchmark(copie, N, sortareOddEvenSecvential, "Odd-Even Sort");
 
-	
+
+	//Test pentru Odd - Even Parallel Sort
+	//Durata sortare = 25.3927
+	//benchmark(copie, N, sortareOddEvenParalel, "Odd-Even Parallel Sort");
+
+
+	//Test pentru Merge Sort
+	//Durata sortare = 0.164617
+	benchmark(copie, N, sortareMerge, "Merge Sort");
+
 }
